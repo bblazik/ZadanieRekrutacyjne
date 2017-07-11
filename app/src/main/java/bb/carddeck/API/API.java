cdde7b21d30fe8f231280198959a63c1a83476e9
bla bla <bartek.blazik@gmail.com>
2017-07-11 14:08:07 +0200

    API prototype

---
+++ app/src/main/java/bb/carddeck/API/API.java
@@ -0,0 +1,54 @@
+package bb.carddeck.API;
+
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.*;
+import retrofit2.http.Query;
+
+public class API {
+
+    private static APIInterface apiInterface;
+    private static String url = "http://deckofcardsapi.com/";
+    public static APIInterface getClient() {
+        if (apiInterface == null) {
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .addInterceptor(
+                            new Interceptor() {
+                                @Override
+                                public Response intercept(Chain chain) throws IOException {
+                                    Request request = chain.request().newBuilder()
+                                            .addHeader("Accept", "application/json").build();
+                                    return chain.proceed(request);
+                                }
+                            }).build();
+
+            Gson gson = new GsonBuilder()
+                    .setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+                    .create();
+
+            Retrofit client = new Retrofit.Builder()
+                    .baseUrl(url)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .client(okHttpClient)
+                    .build();
+            apiInterface = client.create(APIInterface.class);
+        }
+        return apiInterface;
+    }
+    public interface APIInterface {
+
+
+    }
+}
